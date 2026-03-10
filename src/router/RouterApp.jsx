fec01ddb726e3f837d6390b7ce37e30c2e0970fa
mate baldassini <mateobaldasini@gmail.com>
2026-03-10 12:53:19 -0300

    mayuscula de acerca

--- src/router/RouterApp.jsx
+++ src/router/RouterApp.jsx
@@ -14,7 +14,7 @@ const RouterApp = () => {
         <Route path="/" element={<RouteProtected><Home /></RouteProtected>} />
         <Route path="/Login" element={<Login />} />
         <Route path="/registro" element={<Registro />} />
-        <Route path="/acerca.jsx" element={<Acerca />} />
+        <Route path="/Acerca.jsx" element={<Acerca />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
